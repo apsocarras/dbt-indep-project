0fa3ce25d2b223393af87d5fa36e3613149cb853
Alejandro Socarras <apsocarras@gmail.com>
2023-01-10 18:55:39 -0500

    Complete staging model for stg_attendance

--- indep_dbt/models/staging/world_cup_22/stg_attendance.sql
+++ indep_dbt/models/staging/world_cup_22/stg_attendance.sql
@@ -1,6 +1,20 @@
 with src_world_cup_22 as (
 
   select * from {{ source('world_cup_22', 'attendance') }}
+), 
+
+final as (
+
+  select  
+    Date as game_date,
+    CAST(CONCAT(SUBSTR(Time, 1, 5),":00") AS TIME) as game_start, 
+    CAST(CONCAT(SUBSTR(Time, 8, 5), ":00") AS TIME) as game_end, 
+    Home as home, 
+    Away as away, 
+    Attendance as attendance, 
+    Venue as stadium
+
+    from src_world_cup_22
 )
 
-select * from src_world_cup_22
+select * from final
